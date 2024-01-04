89450f1ba8a7587d183d03c3aa46dd1b6f867eae
Piyush Joshi <55031906+inspirepiyush@users.noreply.github.com>
2024-01-04 12:11:36 +0000

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log(" its all good when it is good , and its always bad even its good");
+console.log(" its all good when it is good , and its always bad even its good"); 
+console.log(" i am piyush and i am trying to raise my first pr ")
