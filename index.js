c8cb2f78bfef0388e83e420cdcf2c1914b61220c
Piyush Joshi <55031906+inspirepiyush@users.noreply.github.com>
2024-01-04 12:24:50 +0000

    iuu

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log(" its all good when it is good , and its always bad even its good"); 
 console.log(" i am piyush and i am trying to raise my first pr ")
-console.log(" i am piyush and i am trying to raise my first pr ")
+console.log(" i am piyush and i am trying to raiseguuu my first pr ")
